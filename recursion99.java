3b6e1962fae0156a43bb33597b6746037df9a0f3
mayank4singh <mailatmausam@gmail.com>
2023-01-13 19:25:09 +0530

    fourth commit

---
+++ recursion99.java
@@ -0,0 +1,13 @@
+//How to create Find Factorial No using Recursion Program in java
+
+public class recursion99 {
+    static int factorial(int n){
+    if (n == 1)
+    return 1;
+    else
+    return(n * factorial(n-1));
+    }
+    public static void main(String[] args) { 
+        System.out.println("Factorial of 5 is: "+factorial(5));
+    }
+    }
