1c60e1a089561a2842b0010ee2dfe6d704830680
mayank4singh <mailatmausam@gmail.com>
2023-01-15 18:18:29 +0530

    sixth commit

---
+++ Star18.java
@@ -0,0 +1,12 @@
+class Star18 {
+    public static void main(String[] args) {
+    int row, nos;   //nos= number of stars
+    for (row = 1; row <= 10; row++) {
+        for(nos = 1; nos <= row; nos++) { 
+            System.out.print("*");
+    }
+        System.out.println(); // Go to next line
+        }
+    }
+}
+    
